edce4efc16ae449b1bd656b1b4bf637725918ced
Ruber regino <rjreginov@ul.edu.co>
2019-09-12 17:46:02 -0500

    pracial 2

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,6 +1,6 @@
 import { NgModule } from "@angular/core";
 import {Routes,RouterModule, Router} from "@angular/router";
-import { LoginComponent } from './login/login.component';
+import { LoginComponent } from './pages/settings/login/login.component';
 import { ThemeComponent } from './theme/theme.component';
 export const routes: Routes=[{
     path  : 'login',    
@@ -11,8 +11,17 @@ export const routes: Routes=[{
     component: ThemeComponent,
     children: [
         {
-            path: '',
+            path: 'dashboard',
             loadChildren:'./pages/dashboard/dashboard.module#DashboardModule'
+        },
+        {
+            path  : 'administration',    
+            loadChildren:'./pages/administration/administration.module#AdministrationModule',
+        },
+        {
+            path: 'settings',
+            loadChildren:'./pages/settings/settings.module#SettingsModule'
+
         }
     ]
 
--- src/app/pages/settings/settings-routing.module.ts
+++ src/app/pages/settings/settings-routing.module.ts
@@ -1,8 +1,38 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AreasComponent } from './areas/areas.component';
+import { AuthorsComponent } from './authors/authors.component';
+import { BooksComponent } from './books/books.component';
+import { CategoriesComponent } from './categories/categories.component';
+import { SectionsComponent } from './sections/sections.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+/*primera ruta */
+{
+  path: 'areas',
+  component:AreasComponent
+},
+/**end ruta */
+{
+  path:'authors',
+  component:AuthorsComponent
+  
+},
+{
+  path:'books',
+  component:BooksComponent
+},
+{
+  path:'categories',
+  component:CategoriesComponent
+},
+{
+  path:'sections',
+  component:SectionsComponent
+}
+
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
